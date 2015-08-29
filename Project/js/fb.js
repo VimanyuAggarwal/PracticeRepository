f954fc0c9c12c1801b24b0844b46eec4d0515c7e
VimanyuAggarwal <agg.vim@gmail.com>
2015-08-29 08:37:14 +0530

    WTF

---
+++ Project/js/fb.js
@@ -0,0 +1,8 @@
+FB.api(
+  '/search',
+  'GET',
+  {"q":"delhi","type":"event"},
+  function(response) {
+      CAACEdEose0cBALVS8ZBfSZA6wT55nQ49xRXEjieIYCgriL8wJpDkWvPkT32vXJloNMGtyhYpidFmiqiIgtusfSgiFDRt0qM95Dl2Uj3pspSATOyyRqn9RS6n4x1oFwZAx5IRQ4unBei45yUWb4IRjhXY6Hnu7Ubz1ZBQNLgj0DrHHKvVJnrIMTdwhkCReDqpDZAeMAKqoLXZA42ND6zdNJ
+  }
+);
